f0cf2ae69e19d3da2896c01aee8b9032b43757be
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-04-25 00:48:11 -0700

    Created Singleton Design Pattern

---
+++ SingletonPattern/SingletonPattern/DisplaySize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class DisplaySize
+    {
+        public int Width { get; set; } = 1366;
+
+        public int Height { get; set; } = 768;
+
+        public void Maximazed()
+        {
+            Width = 1366;
+            Height = 768;
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/MockUserSingleton.cs
@@ -0,0 +1,37 @@
+﻿namespace SingletonPattern
+{
+    public class MockUserSingleton
+    {
+        // Bu qismda ayna shu class imizdan private instance olishimiz kerak
+        // Sababi hamma class aynan shu instance ni ishlatadi
+        private static MockUserSingleton _mockUserSingleton;
+
+        // User biror tilni tanlasa u saqlanib qolishi uchun til qo'shib qo'ydik
+        // Ya'ni biror bir tanlansa ushbu class imizning instance si yagona bo'lgani uchun
+        // Projectni hohlagan yerida tilni o'zgartirsak hamma joyida bir hil o'zgaradi
+        public Language Language = Language.En;
+
+        // Ekranni o'lchovlarini saqlab qolish uchun 
+        // Ekranni bo'yi va enini saqlab turuvchi class ochib qoyildi
+        // Ya'ni siz uzingiz hohlagan class yoki enum ... ni qo'shishingiz mumkin
+        public DisplaySize DisplaySize = new DisplaySize();
+
+        // Constructor ni private qilish Singelton Design Patternning asosiy ishi xisoblanadi
+        // Ushbu class dan new MockUserSingleton() qilib hech qayerda instance ola olmaymiz
+        private MockUserSingleton()
+        {
+
+        }
+
+        // Ushbu method ham Singleton Design Patternning asosiy ishlaridan xisoblanadi
+        // Ya'ni qayerda ushbu class objectidan foydalanmoqchi bo'lsak yagona instance ni qaytarib beradi
+        // U public yoki Internal bo'lishi kerak va static qilish kerak
+        // static qilinishini sababi ushbu class imizdan object olib bolmaydi sababi constructor private
+        // Shuning uchun static bolishi kerak
+        public static MockUserSingleton GetInstance()
+        {
+            if (_mockUserSingleton is null) _mockUserSingleton = new MockUserSingleton();
+            return _mockUserSingleton;
+        }
+    }
+}
